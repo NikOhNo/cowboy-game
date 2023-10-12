cc190efe49412fbcb14275445f69be953fc0a51b
NikOhNo <80977626+NikOhNo@users.noreply.github.com>
2023-10-11 23:25:11 -0700

    in progress gun

---
+++ Assets/Scripts/Gameplay/Player/States/Gameplay States/AimState.cs
@@ -0,0 +1,34 @@
+﻿using Assets.Scripts.Gameplay.Player.Movement_States;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Gameplay.Player.States.Gameplay_States
+{
+    public class AimState : GameplayStateBase
+    {
+        float elapsedAimTime = 0f;
+
+        public override void EnterState(PlayerStateController stateController, PlayerController playerController)
+        {
+            base.EnterState(stateController, playerController);
+
+            elapsedAimTime = 0f;
+        }
+
+        public override void PerformState()
+        {
+            // TODO: 
+            // * Walk slow
+            // * Aim at cursor
+            // * Click to fire
+            elapsedAimTime += Time.deltaTime;
+            playerController.gunPivot.rotation = new Quaternion(0, 0, , 0);
+        }
+
+        public override void ExitState()
+        {
+            elapsedAimTime = 0;
+            throw new System.NotImplementedException();
+        }
+    }
+}
