1b8d4756126c26b6073602a6823b1fe8379d14dd
clefalto <113867060+clefalto@users.noreply.github.com>
2024-02-05 17:46:19 -0800

    silly scene manager

---
+++ Assets/Scripts/SceneManaging/SceneSwitcher.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void SwitchScene(SceneAsset newScene)
+    {
+        // SceneManager.LoadScene(scene.buildIndex);
+        var assetPath = AssetDatabase.GetAssetPath(newScene);
+        SceneManager.LoadScene(assetPath);
+        // var scene = SceneManager.GetSceneByPath(assetPath);
+        Debug.Log($"asset path is {assetPath}");
+        // SceneManager.LoadScene(scene.buildIndex);
+    }
+}
