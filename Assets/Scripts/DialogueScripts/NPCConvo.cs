692edb27b61352652f6f9d291039ecbd289bc400
veritasia <aminaapstudent@yahoo.com>
2024-02-21 20:57:30 -0800

    npc prefab done
    YEEHAW YIPPEEE, please make a gameobject to attach the dialogue to and it should work

---
+++ Assets/Scripts/DialogueScripts/NPCConvo.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DialogueEditor;
+
+public class NPCConvo : MonoBehaviour
+{
+    public NPCConversation myConvo;
+    bool canTalk = false;
+    // private void OnMouseOver() {
+    //     if (Input.GetMouseButtonDown(0)) {
+    //         ConversationManager.Instance.StartConversation(myConvo);
+    //     }
+    // }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Player") {
+            canTalk = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.tag == "Player") {
+            canTalk = false;
+        }
+    }
+
+    private void Update() {
+        if (canTalk && (Input.GetKeyDown(KeyCode.F))) {
+            ConversationManager.Instance.StartConversation(myConvo);
+        }
+    }
+}
