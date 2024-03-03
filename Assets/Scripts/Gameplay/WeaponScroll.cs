59bc1f6b16fb9eca365a78c9f65eb697ef15a4fa
Wes <wmont012@ucr.edu>
2024-03-03 14:26:43 -0800

    Basic scrollwheel works

---
+++ Assets/Scripts/Gameplay/WeaponScroll.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponScroll : MonoBehaviour
+{
+    public float scrollSpeed = 10.0f;
+    public int currentValue = 0;
+    public int numWeapons = 3;
+
+    void Update()
+    {
+        // Get the scroll wheel input
+        float scrollInput = Input.GetAxisRaw("Mouse ScrollWheel");
+
+        // Change the current value based on the scroll input
+        float midValue = scrollInput * scrollSpeed;
+        Debug.Log(midValue);
+        currentValue += (int)(midValue);
+        if(currentValue < 0)
+        {
+            currentValue = numWeapons - 1;
+        }
+        else{
+            currentValue = currentValue % numWeapons;
+        }
+        WeaponLogic();
+    }
+
+
+    void WeaponLogic(){
+        for (int i = 0; i < numWeapons; i++)
+        {
+            if (i == currentValue)
+            {
+                transform.GetChild(i).gameObject.SetActive(true);
+            }
+            else
+            {
+                transform.GetChild(i).gameObject.SetActive(false);
+            }
+        }
+    }
+
+}
